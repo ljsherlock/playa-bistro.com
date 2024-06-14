cb1682e2a1b21c2041ab2a674f300021695438d9
ljsherlock <lewis@ljsherlock.com>
2024-06-14 15:19:33 +0700

    fix site title

--- wp-content/themes/playa/header.php
+++ wp-content/themes/playa/header.php
@@ -21,7 +21,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' |'; } ?> <?php bloginfo('name'); ?></title>
 <link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
