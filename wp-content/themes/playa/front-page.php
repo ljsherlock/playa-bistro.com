687a2052925e6fdb0b18a3134d5e3bfcb16bcf4c
ljsherlock <lewis@ljsherlock.com>
2024-06-14 14:15:14 +0700

    completed theme + forms plugin.

---
+++ wp-content/themes/playa/front-page.php
@@ -0,0 +1,19 @@
+<?php 
+
+get_header();
+
+require('templates/navbar.php');
+
+require('templates/site-header.php');
+
+require('templates/reviews.php');
+
+// require('templates/menu.php');
+
+require('templates/paralax.php');
+
+require('templates/contact.php');
+
+get_footer();
+
+?>
